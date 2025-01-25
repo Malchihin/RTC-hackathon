6cf99b2fce6119d988be99682b1d14cfaf2c9c58
Dany <dany.malchihin4@gmail.com>
2025-01-25 20:19:48 +0300

    aruco

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from picamera2 import Picamera2, Preview
 from time import sleep
+import cv2
+import cv2.aruco
  
-camera = Picamera2() 
+camera = Picamera2()
  
 preview_config = camera.create_preview_configuration()
 camera.configure(preview_config)
@@ -9,9 +11,31 @@ camera.configure(preview_config)
 camera.start_preview(Preview.QTGL)
 
 camera.start()
+camera_cv2 = camera.capture_array()
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+arucoParam = cv2.aruco.DetectorParameters()
+arucoDetect = cv2.aruco.ArucoDetector(arucoDict,arucoParam)
 
-sleep(10)
+robotID = 9
+points = [[0,0]]
 
-camera.stop()
+while True:
+    _,img = camera.read(0)
+    try:
+        markerCornes, markerID, markerNo = arucoDetect.detectMarkers(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY))
+        if markerID is not None:
+            if markerID == robotID:
+                ID = str(markerID)
+                cornes = markerCornes[0][0]
+                cornes_x = int((cornes[0][0]+cornes[1][0]+cornes[2][0]+cornes[3][0]) / 4)
+                cornes_y = int((cornes[0][1]+cornes[1][1]+cornes[2][1]+cornes[3][1]) / 4)
+                #print(cornes_y)
+                #print(cornes_x)
+                print(str(markerID))
+    except:points.clear()
+
+    sleep(10)
+
+    camera.stop()
 
 camera.stop_preview()
