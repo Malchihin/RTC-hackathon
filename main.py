2b22e790e7e59e3ebc77128d63060d5b3a6982dc
Dany <dany.malchihin4@gmail.com>
2025-01-27 18:22:03 +0300

    aruco

--- main.py
+++ main.py
@@ -19,16 +19,17 @@ points = [[0, 0]]
 
 
 while True:
-    img = camera.capture_array()
-    markerCorners, markerIDs, _ = arucoDetect.detectMarkers(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-    if markerIDs is not None:
-        for i, markerID in enumerate(markerIDs):
-            if markerID[0] == robotID:
-                corners = markerCorners[i][0]
-                corners_x = int((corners[0][0] + corners[1][0] + corners[2][0] + corners[3][0]) / 4)
-                corners_y = int((corners[0][1] + corners[1][1] + corners[2][1] + corners[3][1]) / 4)
-                print(f"Marker ID: {markerID[0]}, Center: ({corners_x}, {corners_y})")
-    sleep(10)
-
+    try:
+        img = camera.capture_array()
+        markerCorners, markerIDs, _ = arucoDetect.detectMarkers(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+        if markerIDs is not None:
+            for i, markerID in enumerate(markerIDs):
+                if markerID[0] == robotID:
+                    corners = markerCorners[i][0]
+                    corners_x = int((corners[0][0] + corners[1][0] + corners[2][0] + corners[3][0]) / 4)
+                    corners_y = int((corners[0][1] + corners[1][1] + corners[2][1] + corners[3][1]) / 4)
+                    print(f"Marker ID: {markerID[0]}, Center: ({corners_x}, {corners_y})")
+        sleep(10)
+    except:points.clear()
     camera.stop()
     camera.stop_preview()
