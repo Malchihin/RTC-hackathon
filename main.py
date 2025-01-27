548604643eeaca3b528351bb11f692f3fad90d0a
Dany <dany.malchihin4@gmail.com>
2025-01-27 18:14:18 +0300

    aruco

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ camera = Picamera2()
 preview_config = camera.create_preview_configuration()
 camera.configure(preview_config)
 
-camera.start_preview(Preview.QTGL)
+camera.start_preview(Preview.DRM)
 
 camera.start()
 
