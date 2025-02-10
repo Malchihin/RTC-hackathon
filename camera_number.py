f65fa362e0ff6518bf968b4c88276f28a56c2df6
Dany <dany.malchihin4@gmail.com>
2025-02-10 19:38:01 +0300

    1

---
+++ camera_number.py
@@ -0,0 +1,23 @@
+from neronka_proverka import neronka
+from picamera2 import Picamera2, Preview
+from time import sleep
+import cv2
+
+camera = Picamera2()
+
+camera.start_preview()
+
+camera.start()
+
+while True:
+    img = camera.capture_array()
+    image_path = neronka(img)
+
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+camera.stop()
+camera.stop_preview()
+cv2.destroyAllWindows()
