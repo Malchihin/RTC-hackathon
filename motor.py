92b8f23f8e6789bb980e4c3dc4544f7b4e8c9e9b
Dany <dany.malchihin4@gmail.com>
2025-03-24 20:20:32 +0300

    motor code

--- motor.py
+++ motor.py
@@ -1,34 +1,35 @@
 import RPi.GPIO as GPIO
 import time
 
-motor_pin1 = 17  
-motor_pin2 = 27  
+motor_pin1 = 17
+motor_pin2 = 27
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motor_pin1, GPIO.OUT)
 GPIO.setup(motor_pin2, GPIO.OUT)
 
 def forward():
+    print(1)
     GPIO.output(motor_pin1, GPIO.HIGH)
     GPIO.output(motor_pin2, GPIO.LOW)
 
 def backward():
+    print(2)
     GPIO.output(motor_pin1, GPIO.LOW)
     GPIO.output(motor_pin2, GPIO.HIGH)
 
 def stop():
+    print(2)
     GPIO.output(motor_pin1, GPIO.LOW)
     GPIO.output(motor_pin2, GPIO.LOW)
 
-try:
-
+while True:
     forward()
-    time.sleep(2) 
+    time.sleep(2)
     stop()
-    time.sleep(1) 
+    time.sleep(1)
     backward()
-    time.sleep(2)  
+    time.sleep(2)
     stop()
 
-finally:
-    GPIO.cleanup()
+    
