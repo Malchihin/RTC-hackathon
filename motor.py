92f829c62c58a02704c7d92eff9740c1ed7318c7
daa_ras <dany.malchihin4@gmail.com>
2025-04-04 17:31:24 +0300

    speed15

--- motor.py
+++ motor.py
@@ -1,88 +1,79 @@
-    import RPi.GPIO as GPIO
-    import time
+import RPi.GPIO as GPIO
+import time
 
-    motor_pin1 = 17
-    motor_pin2 = 27
+# Определение пинов
+motor_pin1 = 17
+motor_pin2 = 27
+motor2_pin1 = 23
+motor2_pin2 = 24
+servo_pi_motor = 18
 
-    motor2_pin1 = 23
-    motor2_pin2 = 24
+# Настройка GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(motor_pin1, GPIO.OUT)
+GPIO.setup(motor_pin2, GPIO.OUT)
+GPIO.setup(motor2_pin1, GPIO.OUT)
+GPIO.setup(motor2_pin2, GPIO.OUT)
+GPIO.setup(servo_pi_motor, GPIO.OUT)
 
-    servo_pi_motor = 18
+# Создание объектов PWM
+motor1_pwm1 = GPIO.PWM(motor_pin1, 50)
+motor1_pwm2 = GPIO.PWM(motor_pin2, 50)
+motor2_pwm1 = GPIO.PWM(motor2_pin1, 50)
+motor2_pwm2 = GPIO.PWM(motor2_pin2, 50)
+pwm = GPIO.PWM(servo_pi_motor, 50)
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(motor_pin1, GPIO.OUT)
-    GPIO.setup(motor_pin2, GPIO.OUT)
+# Запуск PWM
+motor1_pwm1.start(0)
+motor1_pwm2.start(0)
+motor2_pwm1.start(0)
+motor2_pwm2.start(0)
+pwm.start(0)
 
-    GPIO.setup(motor2_pin1, GPIO.OUT)
-    GPIO.setup(motor2_pin2, GPIO.OUT)
+def forward():
+    print("Forward")
+    motor1_pwm1.ChangeDutyCycle(15)
+    motor1_pwm2.ChangeDutyCycle(0)
+    motor2_pwm1.ChangeDutyCycle(15)
+    motor2_pwm2.ChangeDutyCycle(0)
 
-    motor1_pwm1 = GPIO.PWM(motor_pin1, 100)
-    motor1_pwm2 = GPIO.PWM(motor_pin2, 100)
+def backward():
+    print("Backward")
+    motor1_pwm1.ChangeDutyCycle(0)
+    motor1_pwm2.ChangeDutyCycle(15)
+    motor2_pwm1.ChangeDutyCycle(0)
+    motor2_pwm2.ChangeDutyCycle(15)
 
-    motor2_pwm1 = GPIO.PWM(motor2_pin1, 100)
-    motor2_pwm2 = GPIO.PWM(motor2_pin2, 100)
+def stop():
+    print("Stop")
+    motor1_pwm1.ChangeDutyCycle(0)
+    motor1_pwm2.ChangeDutyCycle(0)
+    motor2_pwm1.ChangeDutyCycle(0)
+    motor2_pwm2.ChangeDutyCycle(0)
 
-    GPIO.setup(servo_pi_motor, GPIO.OUT)
+def set_angle(angle):
+    """Установить угол поворота сервопривода."""
+    duty_cycle = 2 + (angle / 18)  # Преобразование угла в рабочий цикл
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
 
-    pwm = GPIO.PWM(servo_pi_motor, 50)
-    pwm.start(0)
+try:
+    while True:
 
-    motor_pin1.start(0)
-    motor1_pwm2.start(0)
+        forward()
+        time.sleep(5)
 
-    motor_pin1.start(0)
-    motor1_pwm2.start(0)
+        backward()
+        time.sleep(5)
 
-    
-    def forward():
-        print("Forward")
-        motor1_pwm1.ChangeDutyCycle(100)
-        motor1_pwm2.ChangeDutyCycle(0)
-        motor2_pwm1.ChangeDutyCycle(100)
-        motor2_pwm2.ChangeDutyCycle(0)
-    
-    def backward():
-        print("Backward")
-        motor1_pwm1.ChangeDutyCycle(0)
-        motor1_pwm2.ChangeDutyCycle(100)
-        motor2_pwm1.ChangeDutyCycle(0)
-        motor2_pwm2.ChangeDutyCycle(100)
-    
-    def stop():
-        print("Stop")
-        motor1_pwm1.ChangeDutyCycle(0)
-        motor1_pwm2.ChangeDutyCycle(0)
-        motor2_pwm1.ChangeDutyCycle(0)
-        motor2_pwm2.ChangeDutyCycle(0)
+except KeyboardInterrupt:
+    pass
 
-    def set_angle(angle):
-        """Установить угол поворота сервопривода."""
-        duty_cycle = 2 + (angle / 18)  # Преобразование угла в рабочий цикл
-        pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(1)
-
-    try:
-        while True:
-            set_angle(25)
-            time.sleep(0.5)
-
-            forward()
-            print(1)
-            time.sleep(0.5)
-
-            backward()
-            print(2)
-            time.sleep(0.5)
-
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-
+finally:
+    # Остановка PWM и очистка GPIO
+    motor1_pwm1.stop()
+    motor1_pwm2.stop()
+    motor2_pwm1.stop()
+    motor2_pwm2.stop()
     pwm.stop()
-
-    motor_pin1.stop()
-    motor_pin2.stop()
-
-    motor2_pin1.stop()
-    motor2_pin2.stop()
-
     GPIO.cleanup()
